1326a9c54fc578c0bc82948cce743e9bf6e7f2e7
Tortuxo <alexandreducos6@gmail.com>
2024-01-18 11:45:08 +0100

    matplotlib ex 1

--- pem_matplotlib.py
+++ pem_matplotlib.py
@@ -1,1 +1,13 @@
 from matplotlib import pyplot as plt
+import numpy as np
+
+#x = np.linspace(-3,3,1000)
+
+#plt.plot(x,x**2+1)
+#plt.rcParams["figure.figsize"] = (0,0)
+#plt.show()
+
+#x = np.linspace(0,2*np.pi,1000)
+#plt.plot(np.sin(3*x),np.sin(10*x))
+#plt.show()
+
